1de0be2876da8be721a36e11f65bb33d1642144c
srishti00 <93531048+srishti00@users.noreply.github.com>
2022-09-29 14:46:17 +0530

    Add files via upload

---
+++ CaseStudy2/LocalNotificationFramework/LocalNotificationFramework/LocalNotificationFramework.h
@@ -0,0 +1,18 @@
+//
+//  LocalNotificationFramework.h
+//  LocalNotificationFramework
+//
+//  Created by Capgemini-DA204 on 9/25/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalNotificationFramework.
+FOUNDATION_EXPORT double LocalNotificationFrameworkVersionNumber;
+
+//! Project version string for LocalNotificationFramework.
+FOUNDATION_EXPORT const unsigned char LocalNotificationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalNotificationFramework/PublicHeader.h>
+
+
